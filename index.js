0e24530155a391d8556811e2ed0c09c954ec8aeb
AndresPe <julipe.andres@gmail.com>
2019-09-27 20:05:25 -0500

    segundo commit tratando de resolver problemas

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 const express = require('express')
+const app = express()
+
+app.use("/saludo", (req, res, next) =>{
+    console.log("hola desde la raiz");
+     next()
+})
+
+
+const port = 3100
+
+app.listen(port, () =>{
+    console.log("el servidor se ejecuta desde el puerto: "+port);
+})
